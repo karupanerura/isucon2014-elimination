0193d3ff60ab2f677d5db2d703692e057dc274fd
isucon4-ami <karupa@cpan.org>
2014-09-27 04:52:18 +0000

    git pull

--- bench.sh
+++ bench.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-git pull
 # defaut 1 worklaod
 /home/isucon/benchmarker bench --api-key 43-3-ncfwsc-knex-0ce888d78423fc74499c4af51eb51215d51341ad --workload ${1:-1} --init /home/isucon/init.sh
