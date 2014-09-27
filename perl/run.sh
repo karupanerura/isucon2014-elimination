a08b1a040fa4e038b15aeefa44abc3f128827a69
karupanerura <karupa@cpan.org>
2014-09-27 13:40:58 +0900

    Starletにしてみる

--- perl/run.sh
+++ perl/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/isucon/webapp/perl
-exec /home/isucon/env.sh carton exec plackup -s Starman --host localhost:8080 -E prod app.psgi
+exec /home/isucon/env.sh carton exec start_server --port=8080 -- plackup -s Starlet --max-workers 4 --max-reqs-per-child 50000 -E prod app.psgi
