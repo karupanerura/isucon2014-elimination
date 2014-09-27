fbf795a53078e5f138e0300ec80aebeaafa928a6
karupanerura <karupa@cpan.org>
2014-09-27 14:10:22 +0900

    Revert "tcp socketに戻してみる"
    This reverts commit 4ecd896d0f1ac679cdc23e59400d0094e14c9ccc.

--- perl/run.sh
+++ perl/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/isucon/webapp/perl
-exec /home/isucon/env.sh carton exec start_server --port=8080 -- plackup -s Starlet --max-workers 4 --max-reqs-per-child 50000 -E prod app.psgi
+exec /home/isucon/env.sh carton exec start_server --path=/tmp/app.sock -- plackup -s Starlet --max-workers 4 --max-reqs-per-child 50000 -E prod app.psgi
