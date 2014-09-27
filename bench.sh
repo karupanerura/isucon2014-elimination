71db8a6cc36e229daef1d7d0186deefedc5dd7bf
isucon4-ami <karupa@cpan.org>
2014-09-27 04:43:22 +0000

    ベンチマークスクリプトのショートカット

---
+++ bench.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+BASEDIR="/home/isucon/webapp"
+cd ${BASEDIR}
+git pull
+# defaut 1 worklaod
+/home/isucon/benchmarker bench --api-key 43-3-ncfwsc-knex-0ce888d78423fc74499c4af51eb51215d51341ad --workload ${1:-1} --init /home/isucon/init.sh
